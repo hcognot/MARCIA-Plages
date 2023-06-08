d407b1cfca11eb0063ca195bce1265262c8c726e
guerrier8u <helene.cognot3@etu.univ-lorraine.fr>
2023-06-08 16:41:01 +0200

    concateneexlsx: comment and cleaning

--- concatenexlsx.py
+++ concatenexlsx.py
@@ -1,9 +1,19 @@
 import os
 import pandas as pd
-import openpyxl
+# import openpyxl
 import shutil
 from maskSigma import Mask
 
+""" create or replace a plages_'atom'.xlsx file for a given atom and store its plages= ranges inside
+Parameters
+        ----------
+        reponse : 2D numpy array
+            a range (plage)
+        path : str
+            where store the created file
+        indice_nom  : str
+            name of the atom 
+        """
 def createAtomFile(reponse, path, indice_nom):
     """ exportation des plages en . xlsx"""
     flattened_data = [' - '.join(map(str, pair)) for pair in reponse]
@@ -21,9 +31,7 @@ def createAtomFile(reponse, path, indice_nom):
     current_directory = os.getcwd()
 
     split_strings =path.split("\\")
-    # first_string = split_strings[0]
-    # second_string = split_strings[1]
-
+    
     destination_directory = os.path.join(current_directory, split_strings[0], split_strings[1])
 
     # Remove the file if it already exists in the destination directory
@@ -33,15 +41,16 @@ def createAtomFile(reponse, path, indice_nom):
 
     shutil.move(nom_fichier, destination_directory)
 
+""" create or replace the combined_file.xlsx file from all the existing plages_'atom'.xlsx 
+Parameters
+        ----------
+        relative_path : str
+            relative path, where the plages_'atom'.xlsx come from and where combined_file.xlsx is placed
+        """
 def createSynthesisFile (relative_path):
 
-
     """ adding all atomic data into a single . xls file """
-    # Create a new folder
-    # folder_name = "lines_folder"
-    # os.makedirs(folder_name, exist_ok=True)
     split_strings =relative_path.split("/")
-    directory = '/'.join(split_strings)
     current_directory= os.getcwd()
 
     print(os.getcwd())
@@ -57,12 +66,9 @@ def createSynthesisFile (relative_path):
         print(file_name)
 
     # List the .xlsx files beginning by 'plages' in the current directory
-    # xlsx_files = [file for file in  os.listdir(directory) if file.endswith('.xlsx')]
     xlsx_files = [file for file in  os.listdir(target_directory) if file.startswith('plages')]
     print("Filtered XLSX files:")
     print(xlsx_files)
-    # xlsx_files = [file for file in  os.listdir(directory) if file.endswith('.xlsx') and file.startswith('plages')]
-    # xlsx_files = [file for file in  os.listdir(split_strings[0]+'/'+split_strings[1]) if file.endswith('.xlsx') and file.startswith('plages')]
     # Create an empty DataFrame to store the combined lines
     combined_data = pd.DataFrame()
 
@@ -71,9 +77,7 @@ def createSynthesisFile (relative_path):
         # Read the file and extract the single column
         file_path = os.path.join(target_directory, file)  # Full file path
         data = pd.read_excel(file_path, header=None).iloc[:, 0]
-        # Read the file and extract the single column
-        # data = pd.read_excel(file, header=None).iloc[:, 0]
-    
+            
         # Append the column to the combined_data DataFrame
         combined_data = pd.concat([combined_data, data], axis=1)
 
@@ -81,15 +85,12 @@ def createSynthesisFile (relative_path):
 
     # Export the combined data to a new file
     combined_file = 'combined_file.xlsx'  # Specify the name of the combined file
-    # combined_data.to_excel(combined_file, index=False, header=False)
     combined_file_path = os.path.join(target_directory, combined_file)
     
     # Remove the existing combined file if it exists
     if os.path.exists(combined_file_path):
         os.remove(combined_file_path)
 
-    # if os.path.exists(combined_file):
-    #     os.remove(combined_file)
     # Export the transposed combined data to a new file
     combined_file = 'combined_file.xlsx'  # Specify the name of the combined file
     data_transposed.to_excel(combined_file, index=False, header=False)
@@ -101,5 +102,3 @@ def createSynthesisFile (relative_path):
     split_strings =relative_path.split('\\')
     destination_directory = os.path.join(current_directory, split_strings[0], split_strings[1])
     shutil.move(combined_file, destination_directory)
-
-    # data_transposed.to_excel(combined_file, index=False, header=False, mode='w')

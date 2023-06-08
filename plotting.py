c408c06fa1f473da7d7df1516a799d9d734aae43
guerrier8u <helene.cognot3@etu.univ-lorraine.fr>
2023-06-08 16:23:55 +0200

    plotting: comment

--- plotting.py
+++ plotting.py
@@ -2,13 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 """Fonctions de tracé"""  
-
+""" plotting of a (x, y) set of points with a label"""
 def trace(x_data, y_data, texte) :
     plt.figure(figsize=(6, 4))
     plt.scatter(x_data, y_data, label=texte)
     plt.legend(loc='best')
 
-""" Fonction de tracé : données initiales et courbe ajustée """    
+""" Fonction de tracé : données initiales et courbe ajustée """ 
+""" plotting of a (x, y) set of points alongside a function , with a label """   
 def trace_ajuste(x_data, y_data, y_calc, texte) :
     plt.figure(figsize=(6, 4))
     plt.scatter(x_data, y_data, label=texte)

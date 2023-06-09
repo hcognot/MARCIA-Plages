20315e2b47b44fcfd2da172a14afdaae7a40c5ca
guerrier8u <helene.cognot3@etu.univ-lorraine.fr>
2023-06-09 11:02:46 +0200

    test : comment (ref Meyer)

--- test.py
+++ test.py
@@ -2,6 +2,12 @@
 import matplotlib.pyplot as plt
 from maskSigma import Mask
 
+"""  ***********************************************************************
+        synthesis : produce the mineral caracterisation of a given sample
+                           (cf MARCIA from H. Meyer) 
+                            adapt it to your needs
+     ***********************************************************************    """
+
 cm = Mask('examples/Data/lead_ore_','.bmp','examples/Data/Mask.xlsx')
 cm.load_table()
 cm.datacube_creation()
